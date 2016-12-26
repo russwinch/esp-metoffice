f308679f9f513e8a62da15f2e87f47dbb8320f69
russ <russell.winch@gmail.com>
2016-12-26 13:40:15 +0000

    implemented wifi manager and rain forecast

--- esp-metoffice.ino
+++ esp-metoffice.ino
@@ -8,6 +8,7 @@
   - Met Office code definitions for weather types: http://www.metoffice.gov.uk/datapoint/support/documentation/code-definitions
   - Tested on Wemos D1 V2 with oled sheild
   - Using the modified version of the adafruit OLED driver by mcauser: https://github.com/mcauser/Adafruit_SSD1306/tree/esp8266-64x48
+  - Using the fantastic WiFiManager to connect wifi, present AP and save settings to FS: https://github.com/tzapu/WiFiManager
 
   /*********************************************************************
   This is an example for our Monochrome OLEDs based on SSD1306 drivers
@@ -27,18 +28,26 @@
   All text above, and the splash screen must be included in any redistribution
 *********************************************************************/
 
-/***TODO***
-  decide which forecast to show : day / night / next day
-  ensure blank data is not returned and use previous data if it is (look for a location?)
-  identify date and time of next rain
-  ability to determine / set the current location (by IP ? )
-  produce more icons
-  implement reading from ldr to adjust led brightness
-  implement timeout to turn the oled off after a delay
-  add mode to show last data dates and times
+/****TO DO****
+  - decide which forecast to show : day / night / next day
+  - ensure blank data is not returned and use previous data if it is (look for a location?)
+  - identify date and time of next rain forecast - add 3hourly forecast?
+  - produce more icons - find a set and batch all when size determined
+  - implement reading from ldr to adjust led brightness
+  - implement timeout to turn the oled off after a delay
+  - add mode to show last data dates and times
+  - animate icons
+  - add more logging to oled screen and implement logging bool
+  - determine current date and time and compare to rain forecast to filter old data
+  - led feedback on updates (switchable)
+
+  - ability to determine / set the current location (by IP ? )
+  - implement weather warnings  
+  - postcode to coordinate search with postcodes.io
+  - look up nearest sites using siteslist and pythag
+  - tidy up the use of strings for the forecast requests
 
 */
-
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <Wire.h>
@@ -53,26 +62,29 @@
 #include "credentials.h" //credentials file with wifi settings and Met Office API key
 #include "bitmaps.h" //bitmaps file
 
-// SCL GPIO5 (D1)
-// SDA GPIO4 (D2)
-#define OLED_RESET D3  // GPIO0
-Adafruit_SSD1306 display(OLED_RESET);
+//enable logging
+bool logging = true;
+//bool logging = false;
 
-//pin definitions
+// pin definitions
 const int redPin = D7;
 const int greenPin = D6;
 const int bluePin = D5;
 const int switchPin = D8; //built in pull-down
 const int ldrPin = A0;
+// SCL GPIO5 (D1)
+// SDA GPIO4 (D2)
+#define OLED_RESET D3  // GPIO0
 
-// Met Office settings
-#define METOFFICE "datapoint.metoffice.gov.uk"
-#define MO_FOREID "351713" //forecast site ID: Hackney
-#define MO_OBVID "3672" //observation site ID: Northolt
-// To obtain a forecast siteID, call the following to return JSON of all sites:
-// http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=<APIKey>
-// To obtain an observation siteID, call the following to return JSON of all sites:
-// http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/sitelist?key=<APIKey>
+Adafruit_SSD1306 display(OLED_RESET);
+
+//define your default values here, if there are different values in config.json, they are overwritten.
+char forecast_id[8]; //forecast site ID
+char observation_id[6]; //observation site ID
+char api_key[40]; //api key
+
+//wifi management
+#include "wifiManagement.h"
 
 /* Met Office fair use policy:
   You may make no more than 5000 data requests per day; and
@@ -84,24 +96,8 @@ long updateDelay = (1 * 60000);
 // Met office weather types
 const char *weatherTypes[] = {"Clear night", "Sunny day", "Partly cloudy (night)", "Partly cloudy (day)", "Not used", "Mist", "Fog", "Cloudy", "Overcast", "Light rain shower (night)", "Light rain shower (day)", "Drizzle", "Light rain", "Heavy rain shower (night)", "Heavy rain shower (day)", "Heavy rain", "Sleet shower (night)", "Sleet shower (day)", "Sleet", "Hail shower (night)", "Hail shower (day)", "Hail", "Light snow shower (night)", "Light snow shower (day)", "Light snow", "Heavy snow shower (night)", "Heavy snow shower (day)", "Heavy snow", "Thunder shower (night)", "Thunder shower (day)", "Thunder"};
 
-// HTTP requests
-const char FORECAST_REQ[] =
-  "GET /public/data/val/wxfcs/all/json/" MO_FOREID "?res=daily&key=" MO_API_KEY " HTTP/1.1\r\n"
-  //  "GET /public/data/val/wxfcs/all/json/" MO_SITEID "?res=3hourly&key=" MO_API_KEY " HTTP/1.1\r\n" ///testing 3 hourly
-  "User-Agent: ESP8266/0.1\r\n"
-  "Accept: */*\r\n"
-  "Host: " METOFFICE "\r\n"
-  "Connection: close\r\n"
-  "\r\n";
-
-const char OBSERVATION_REQ[] =
-  "GET /public/data/val/wxobs/all/json/" MO_OBVID "?res=hourly&key=" MO_API_KEY " HTTP/1.1\r\n"
-  //  "GET /public/data/val/wxfcs/all/json/" MO_SITEID "?res=3hourly&key=" MO_API_KEY " HTTP/1.1\r\n" ///testing 3 hourly
-  "User-Agent: ESP8266/0.1\r\n"
-  "Accept: */*\r\n"
-  "Host: " METOFFICE "\r\n"
-  "Connection: close\r\n"
-  "\r\n";
+// Met Office settings
+#define METOFFICE "datapoint.metoffice.gov.uk"
 
 // current date & time from http request
 String timeStamp;
@@ -122,17 +118,23 @@ int nightPProb;
 // millis() of last update attempt
 unsigned long lastUpdateAttempt;
 
-//modes
+// modes
 int currentMode = 0;
 int totalModes = 3;
+
+// switch
 bool switchReading;
 bool lastSwitchReading;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
 
+// leds
+int RGB;
+//int targetRGB;
+
 void setup() {
   //inputs
-  pinMode (ldrPin, INPUT);
+  //  pinMode (ldrPin, INPUT);
   pinMode (switchPin, INPUT);
 
   //outputs
@@ -141,7 +143,7 @@ void setup() {
   pinMode (bluePin, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  //leds off
+  //turn leds off
   analogWrite(LED_BUILTIN, 1023);
   updateRGB(0);
 
@@ -156,57 +158,28 @@ void setup() {
   display.display();
   delay(500);
   display.clearDisplay(); // Clear the buffer.
-
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-
-  // Connect to WiFi network
-  Serial.println();
-  Serial.print(F("Connecting to "));
-  Serial.println(ssid);
-
-  display.print("Connecting to ");
-  display.println(ssid);
   display.display();
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(F("."));
-    display.print(".");
-    display.display();
-  }
-  Serial.println();
-  Serial.println(F("WiFi connected"));
-  Serial.println(F("IP address: "));
-  Serial.println(WiFi.localIP());
+  if (manageWifi()); //launch wifi using WifiManager
 
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println("Connected.");
-  display.println();
-  display.println("IP address");
-  display.println(WiFi.localIP());
-  display.display();
+  //*****testing JSON read
+  Serial.println("values from JSON");
+  Serial.print("forecast ID: "); Serial.println(forecast_id); //forecast site ID
+  Serial.print("observation ID: "); Serial.println(observation_id); //observation site ID
+  Serial.print("API key: "); Serial.println(api_key); //api key
 
   if (updateData()) updateDisplay(0);
 }
 
 void loop() {
-  switchReading = digitalRead(switchPin);
-  if ((switchReading != lastSwitchReading) && ((millis() - lastDebounceTime) > debounceDelay)) {
-    lastSwitchReading = switchReading;
-    lastDebounceTime = millis();
-    if (switchReading == HIGH) {
-      currentMode++;
-      if (currentMode > (totalModes - 1)) currentMode = 0;
-      updateDisplay(currentMode);
-      //      Serial.print("mode: ");
-      //      Serial.println(currentMode);
-    }
+  if (readSwitch() == HIGH) {
+    currentMode++;
+    if (currentMode > (totalModes - 1)) currentMode = 0;
+    updateDisplay(currentMode);
   }
-
   if (millis() - lastUpdateAttempt > updateDelay) {
     if (updateData()) updateDisplay(currentMode);
     lastUpdateAttempt = millis();
@@ -214,16 +187,59 @@ void loop() {
 }
 
 bool updateData() {
-  char* response;
-  response = httpReq(FORECAST_REQ);
+  String OBSERVATION_REQ =
+    "GET /public/data/val/wxobs/all/json/" + String(observation_id) + "?res=hourly&key=" + String(api_key) + " HTTP/1.1\r\n"
+    "User-Agent: ESP8266/0.1\r\n"
+    "Accept: */*\r\n"
+    "Host: " METOFFICE "\r\n"
+    "Connection: close\r\n"
+    "\r\n";
+
+  String FORECAST_REQ =
+    "GET /public/data/val/wxfcs/all/json/" + String(forecast_id) + "?res=daily&key=" + String(api_key) + " HTTP/1.1\r\n"
+    "User-Agent: ESP8266/0.1\r\n"
+    "Accept: */*\r\n"
+    "Host: " METOFFICE "\r\n"
+    "Connection: close\r\n"
+    "\r\n";
+
+  String HOUR_FORECAST_REQ =
+    "GET /public/data/val/wxfcs/all/json/" + String(forecast_id) + "?res=3hourly&key=" + String(api_key) + " HTTP/1.1\r\n"
+    "User-Agent: ESP8266/0.1\r\n"
+    "Accept: */*\r\n"
+    "Host: " METOFFICE "\r\n"
+    "Connection: close\r\n"
+    "\r\n";
+
+  String FORECAST_SITE_REQ =
+    "GET /public/data/val/wxfcs/all/json/sitelist?key=" + String(api_key) + " HTTP/1.1\r\n"
+    "User-Agent: ESP8266/0.1\r\n"
+    "Accept: */*\r\n"
+    "Host: " METOFFICE "\r\n"
+    "Connection: close\r\n"
+    "\r\n";
+
+  //    char* response = httpReq(FORECAST_SITE_REQ.c_str());
+  char* response = httpReq(FORECAST_REQ.c_str());
   if (forecast(response)) {
-    response = httpReq(OBSERVATION_REQ);
-    if (observation(response)) return true;
-  } else return false;
+    response = httpReq(OBSERVATION_REQ.c_str());
+    if (observation(response)) {
+      response = httpReq(HOUR_FORECAST_REQ.c_str());
+      if (hour_forecast(response)) {
+        confirm(9);
+        return true;
+      }
+    } else {
+      confirm(1);
+      return false;
+    }
+  }
 }
 
 char* httpReq (const char* req) {
-  static char respBuf[4096]; // response buffer for the JSON
+  //  static char respBuf[25096]; // response buffer for the JSON
+  static char respBuf[5096]; // response buffer for the JSON
+  //  static char respBuf[4096]; // response buffer for the JSON
 
   Serial.println();
   Serial.print(F("Connecting to "));
@@ -253,12 +269,11 @@ char* httpReq (const char* req) {
     if (skip_headers) {
       String aLine = httpclient.readStringUntil('\n');
       Serial.println(aLine);
-      if (aLine.substring(0, 4) == "Date") timeStamp = aLine.substring(6); //looking for Date at the start of the line
+      if (aLine.substring(0, 4) == "Date") timeStamp = aLine.substring(6); //looking for "Date" at the start of the line
       if (aLine.length() <= 1) { // Blank line denotes end of headers
         skip_headers = false;
       }
-    }
-    else {
+    } else {
       int bytesIn;
       bytesIn = httpclient.read((uint8_t *)&respBuf[respLen], sizeof(respBuf) - respLen);
       Serial.print(F("bytesIn ")); Serial.println(bytesIn);
@@ -281,36 +296,40 @@ char* httpReq (const char* req) {
     //    delay(DELAY_ERROR);
     //    return; ********************************** FIX THIS
   }
-  // Terminate the C string
-  respBuf[respLen++] = '\0';
+  respBuf[respLen++] = '\0'; // Terminate the C string
   Serial.print(F("respLen "));
   Serial.println(respLen);
   //Serial.println(respBuf);
   return respBuf;
 }
 
-bool forecast(char *json) {
+JsonObject& parseJson(char *j) {
   //  StaticJsonBuffer<3*1024> jsonBuffer;
   DynamicJsonBuffer jsonBuffer; //not recommended but seems to work
 
   // Skip characters until first '{' found
   // Ignore chunked length, if present
-  char *jsonstart = strchr(json, '{');
+  char *jsonstart = strchr(j, '{');
   Serial.print(F("jsonstart :")); Serial.println(jsonstart);
   if (jsonstart == NULL) {
     Serial.println(F("JSON data missing"));
-    return false;
+    //    return false;
   }
-  json = jsonstart;
+  j = jsonstart;
 
   // Parse JSON
-  JsonObject& root = jsonBuffer.parseObject(json);
-  if (!root.success()) {
+  JsonObject& r = jsonBuffer.parseObject(j);
+  if (!r.success()) {
     Serial.println(F("jsonBuffer.parseObject() failed"));
-    return false;
+    //    return false;
   }
   Serial.println("parsed successfully");
   Serial.println();
+  return r;
+}
+
+bool forecast(char *json) {
+  JsonObject& root = parseJson(json);
 
   // Extract weather info from parsed JSON
   JsonObject& DV = root["SiteRep"]["DV"];
@@ -334,7 +353,7 @@ bool forecast(char *json) {
       Serial.print(" : ");
       Serial.println(it->value.asString());
     }
-    //print day forecast
+    //print night forecast
     Serial.println();
     for (JsonObject::iterator it = todayNight.begin(); it != todayNight.end(); ++it) {
       Serial.print(it->key);
@@ -361,28 +380,77 @@ bool forecast(char *json) {
   }
 }
 
+bool hour_forecast(char *json) {
+  JsonObject& root = parseJson(json);
+
+  // Extract weather info from parsed JSON
+  JsonObject& DV = root["SiteRep"]["DV"];
+  JsonObject& location = root["SiteRep"]["DV"]["Location"];
+  JsonObject& period = root["SiteRep"]["DV"]["Location"]["Period"];//[0]["Rep"][0];
+
+  Serial.println("Rain forecast");
+  Serial.println(timeStamp); //current date and time
+  String siteName = location["name"];
+  if (siteName.length() != 0) {
+    Serial.print("Location: "); Serial.println(siteName);
+    String dataTimeStamp = DV["dataDate"];
+    //  Serial.print("Data timestamp: "); Serial.println(dataTimeStamp);
+    Serial.print("Data date: "); Serial.println(dataTimeStamp.substring(0, 10));
+    Serial.print("Data time: "); Serial.println(dataTimeStamp.substring(11, 16));
+
+    int rainTime; //last observation with weather type
+    String rainDate;
+    bool checking = true;
+    for (int d = 0; d < 5; d++) {
+      for (int a = 0; a < 8; a++) {
+        if (checking) {
+          JsonObject& temp = root["SiteRep"]["DV"]["Location"]["Period"][d]["Rep"][a];
+          if (temp["W"] >= 9) { //
+            rainTime = temp["$"]; //time of rain
+            rainDate = root["SiteRep"]["DV"]["Location"]["Period"][d]["value"].asString();
+            checking = false;
+          }
+        }
+      }
+    }
+
+    if (checking) Serial.println("No rain forecast in next 5 days");
+    else {
+      Serial.print("Rain time: "); Serial.println((rainTime / 60) * 100);
+
+      //    JsonObject& rainDay = root["SiteRep"]["DV"]["Location"]["Period"][rainDate];
+      Serial.print("Rain date: "); Serial.println(rainDate);
+      //    Serial.println(rainDay["type"]);
+
+      //    print rain forecast
+      //    for (JsonObject::iterator it = rainDay.begin(); it != rainDay.end(); ++it) {
+      //      Serial.print(it->key);
+      //      Serial.print(" : ");
+      //      Serial.println(it->value.asString());
+      //    }
+    }
+
+    //    dayMaxTemp = todayDay["Dm"];
+    //    Serial.print("Max temp: "); Serial.print(dayMaxTemp); Serial.println(" deg C");
+    //    nightMinTemp = todayNight["Nm"];
+    //    Serial.print("Min temp: "); Serial.print(nightMinTemp); Serial.println(" deg C");
+    //    dayType = todayDay["W"];
+    //    Serial.print("Day weather type: "); Serial.println(weatherTypes[dayType]);
+    //    nightType = todayNight["W"];
+    //    Serial.print("Night weather type: "); Serial.println(weatherTypes[nightType]);
+    //    dayPProb = todayDay["PPd"];
+    //    Serial.print("Day precipitation probability: "); Serial.print(dayPProb); Serial.println("%");
+    //    nightPProb = todayNight["PPn"];
+    //    Serial.print("Night precipitation probability: "); Serial.print(nightPProb); Serial.println("%");
+    return true;
+  } else {
+    Serial.println("JSON empty - no data returned");
+    return false;
+  }
+}
+
 bool observation(char *json) {
-  //  StaticJsonBuffer<3*1024> jsonBuffer;
-  DynamicJsonBuffer jsonBuffer; //not recommended but seems to work
-
-  // Skip characters until first '{' found
-  // Ignore chunked length, if present
-  char *jsonstart = strchr(json, '{');
-  Serial.print(F("jsonstart :")); Serial.println(jsonstart);
-  if (jsonstart == NULL) {
-    Serial.println(F("JSON data missing"));
-    return false;
-  }
-  json = jsonstart;
-
-  // Parse JSON
-  JsonObject& root = jsonBuffer.parseObject(json);
-  if (!root.success()) {
-    Serial.println(F("jsonBuffer.parseObject() failed"));
-    return false;
-  }
-  Serial.println("parsed successfully");
-  Serial.println();
+  JsonObject& root = parseJson(json);
 
   // Extract weather info from parsed JSON
   JsonObject& DV = root["SiteRep"]["DV"];
@@ -404,7 +472,7 @@ bool observation(char *json) {
       if (checking) {
         JsonObject& temp = root["SiteRep"]["DV"]["Location"]["Period"][1]["Rep"][a];
         if (!temp.containsKey("$")) { //the last observation has been passed
-          //add something here to identify last observation with "W" as often missing
+          //****add something here to identify and move back to last observation with "W", as often missing
           last = a - 1;
           checking = false;
         }
@@ -441,8 +509,7 @@ void updateDisplay(int displayMode) {
       display.clearDisplay();
       display.setTextSize(2);
       display.setTextColor(WHITE);
-      if (currentType == 2 || currentType == 3 || currentType == 7) { //if cloudy
-        display.drawBitmap(0, 0, cloudy, 48, 32, 1);
+      if (bitmapDisplay(currentType)) {
         display.setCursor(0, 34);
       } else {
         display.setCursor(0, 0);
@@ -460,8 +527,7 @@ void updateDisplay(int displayMode) {
       display.clearDisplay();
       display.setTextSize(2);
       display.setTextColor(WHITE);
-      if (dayType == 2 || dayType == 3 || dayType == 7) { //if cloudy
-        display.drawBitmap(0, 0, cloudy, 48, 32, 1);
+      if (bitmapDisplay(dayType)) {
         display.setCursor(0, 34);
       } else {
         display.setCursor(0, 0);
@@ -474,14 +540,12 @@ void updateDisplay(int displayMode) {
       display.display();
       break;
 
-
     case 2: // night forecast
       updateRGB(3); //green
       display.clearDisplay();
       display.setTextSize(2);
       display.setTextColor(WHITE);
-      if (nightType == 2 || nightType == 3 || nightType == 7) { //if cloudy
-        display.drawBitmap(0, 0, cloudy, 48, 32, 1);
+      if (bitmapDisplay(nightType)) {
         display.setCursor(0, 34);
       } else {
         display.setCursor(0, 0);
@@ -494,6 +558,16 @@ void updateDisplay(int displayMode) {
       display.display();
       break;
   }
+}
+
+bool bitmapDisplay(int weather) {
+  if (weather == 2 || weather == 3 ) { // partly cloudy (day/night) ***add seperate night version with moon in place of sun
+    display.drawBitmap(0, 0, partlyCloudy, 40, 32, 1);
+    return true;
+  } else if (weather == 7 || weather == 8 ) { // cloudy / overcast
+    display.drawBitmap(0, 0, cloudy, 48, 32, 1);
+    return true;
+  } else return false;
 }
 
 //RGB led control
@@ -534,5 +608,27 @@ void updateRGB (int newColour) {
   analogWrite(redPin, 1023 - r);
   analogWrite(greenPin, 1023 - g);
   analogWrite(bluePin, 1023 - b);
+  RGB = newColour;
 }
 
+void confirm(int c) {
+  updateRGB(0);
+  delay(10);
+  updateRGB(c); //colour
+  delay(10);
+  updateRGB(RGB); //***back to original colour
+}
+
+//void printBoth(char *m) {
+//  Serial.println(m);
+//  display.println(m);
+//}
+
+bool readSwitch() {
+  switchReading = digitalRead(switchPin);
+  if ((switchReading != lastSwitchReading) && ((millis() - lastDebounceTime) > debounceDelay)) {
+    lastSwitchReading = switchReading;
+    lastDebounceTime = millis();
+    return switchReading;
+  } else return false;
+}
---
+++ wifiManagement.h
@@ -0,0 +1,208 @@
+//needed for library
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+
+//libraries already included in main sketch
+//#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+//#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+
+////define your default values here, if there are different values in config.json, they are overwritten.
+//char forecast_id[8]; //forecast site ID
+//char observation_id[6]; //observation site ID
+//char api_key[40]; //api key
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//time to show messages
+int msgDel = 100;
+
+void logMsg(char *m, char *n) {
+  display.clearDisplay(); // Clear the buffer.
+  display.setCursor(0, 0);
+  display.println(m);
+  display.println(n);
+  display.display();
+  Serial.println(m);
+  Serial.println(n);
+  delay(msgDel);
+}
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  display.clearDisplay(); // Clear the buffer.
+  display.setCursor(0, 0);
+  display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.println("SSID");
+  display.setTextColor(WHITE, BLACK); // normal text
+  display.println(ap_ssid);
+  display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.println("Password");
+  display.setTextColor(WHITE, BLACK); // normal text
+  display.println(ap_password);
+  display.display();
+
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
+bool manageWifi() {
+  //  Serial.begin(115200);
+
+  //clean FS, for testing
+  //  SPIFFS.format();
+
+  //  display.clearDisplay(); // Clear the buffer.
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  //  display.setCursor(0, 0);
+
+  //read configuration from FS json
+  if (logging) logMsg("1/4.", "mounting FS...");
+  if (SPIFFS.begin()) {
+    if (logging) logMsg("2/4.", "mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      if (logging) logMsg("3/4.", "reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        if (logging) logMsg("4/4.", "opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(forecast_id, json["forecast_id"]);
+          strcpy(observation_id, json["observation_id"]);
+          strcpy(api_key, json["api_key"]);
+          
+        } else {
+          if (logging) logMsg("failed to load json config", "");
+          //catch error***
+          return false;
+        }
+      }
+    }
+  } else {
+    if (logging) logMsg("failed to mount FS", "");
+    //catch error***
+    return false;
+  }
+  //end read
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter metoffice_text("<h3>Met Office details</h3><b>Forecast ID</b>");
+  WiFiManagerParameter custom_forecast_id("forecast_id", "forecast id", forecast_id, 8);
+  WiFiManagerParameter forecast_text("<br>To obtain a forecast site ID, call the following to return JSON of all sites: http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=YOUR_API_Key<br><br><b>Observation ID</b>");
+  WiFiManagerParameter custom_observation_id("observation_id", "observation id", observation_id, 16, "Obv ID");
+  WiFiManagerParameter observation_text("<br>To obtain an observation site ID, call the following to return JSON of all sites: http://datapoint.metoffice.gov.uk/public/data/val/wxobs/all/json/sitelist?key=YOUR_API_Key<br><br><b>API Key</b>");
+  WiFiManagerParameter custom_api_key("api_key", "api key", api_key, 40);
+  WiFiManagerParameter api_text("<br>Register for Met Office Datapoint here: <a href='http://www.metoffice.gov.uk/datapoint'>metoffice.gov.uk/datapoint</a>");
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //set static ip
+  //  wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+  //add all your parameters here
+  wifiManager.addParameter(&metoffice_text);
+  wifiManager.addParameter(&custom_forecast_id);
+  wifiManager.addParameter(&forecast_text);
+  wifiManager.addParameter(&custom_observation_id);
+  wifiManager.addParameter(&observation_text);
+  wifiManager.addParameter(&custom_api_key);
+  wifiManager.addParameter(&api_text);
+
+  //reset settings - for testing
+  //    wifiManager.resetSettings();
+
+  //set minimu quality of signal so it ignores AP's under that quality
+  //defaults to 8%
+  //wifiManager.setMinimumSignalQuality();
+
+  //sets timeout until configuration portal gets turned off
+  //useful to make it all retry or go to sleep
+  //in seconds
+    wifiManager.setTimeout(600);
+//  wifiManager.setTimeout(120);
+
+
+  wifiManager.setAPCallback(configModeCallback);
+
+  //go into AP mode if button held down at startup
+  if (digitalRead(switchPin) == HIGH) wifiManager.startConfigPortal(ap_ssid, ap_password);
+  
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //and goes into a blocking loop awaiting configuration
+  else if (!wifiManager.autoConnect(ap_ssid, ap_password)) {
+    if (logging) logMsg("failed to connect and hit timeout", "");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  //if you get here you have connected to the WiFi
+  if (logging) logMsg("connected.", "");
+
+  //read updated parameters
+  strcpy(forecast_id, custom_forecast_id.getValue());
+  strcpy(observation_id, custom_observation_id.getValue());
+  strcpy(api_key, custom_api_key.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    if (logging) logMsg("saving config.", "");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["forecast_id"] = forecast_id;
+    json["observation_id"] = observation_id;
+    json["api_key"] = api_key;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      //catch error***
+      if (logging) logMsg("failed to open config file for writing", "");
+      return false;
+    }
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+  if (logging) {
+    display.println("local ip");
+    display.println(WiFi.localIP());
+    display.display();
+    Serial.println("local ip.");
+    Serial.println(WiFi.localIP());
+    delay(msgDel);
+  }
+  // if (logging) logMsg("local ip", WiFi.localIP()); //doesn't work :(
+  return true;
+}
+
+
+
